f8e9de869cfd24a31a865bd795410f2ee5828fe6
Rafael Buranelo Biasotto <rafael.biasotto@solonetwork.com.br>
2026-06-20 08:02:51 -0300

    Add public ip das vm no output

--- module_vm/outputs.tf
+++ module_vm/outputs.tf
@@ -12,3 +12,18 @@ output "vm_private_ip_data_cin" {
   description = "IP privado da VM data em Central India"
   value       = azurerm_network_interface.nic_data_cin.private_ip_address
 }
+
+output "vm_public_ip_fend_cus" {
+  description = "IP público da VM frontend em Central US"
+  value       = azurerm_network_interface.nic_fend_cus.public_ip_address
+}
+
+output "vm_public_ip_bend_cus" {
+  description = "IP público da VM backend em Central US"
+  value       = azurerm_network_interface.nic_bend_cus.public_ip_address
+}
+
+output "vm_public_ip_data_cin" {
+  description = "IP público da VM data em Central India"
+  value       = azurerm_network_interface.nic_data_cin.public_ip_address
+}
