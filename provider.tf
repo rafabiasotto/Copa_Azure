cb91e3768af929a3d3cb7820657c32468afea6aa
Rafael Buranelo Biasotto <rafael.biasotto@solonetwork.com.br>
2026-06-22 13:56:34 -0300

    ajuste imagem readme

--- provider.tf
+++ provider.tf
@@ -1,21 +1,17 @@
 terraform {
-  # Versão mínima do Terraform necessária para executar o projeto
   required_version = ">= 1.15.5"
 
   required_providers {
-    # Provider utilizado para criar e gerenciar os recursos no Azure
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "4.75.0"
     }
 
-    # Provider utilizado para gerar a chave privada da conta ACME
     tls = {
       source  = "hashicorp/tls"
       version = ">= 4.0.0, < 5.0.0"
     }
 
-    # Provider utilizado para emitir o certificado pelo Let's Encrypt
     acme = {
       source  = "vancluever/acme"
       version = ">= 2.16.1, < 3.0.0"
@@ -23,12 +19,22 @@ terraform {
   }
 }
 
-# Configuração principal do provider AzureRM
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy               = true
+      purge_soft_deleted_certificates_on_destroy = true
+      purge_soft_deleted_keys_on_destroy         = true
+      purge_soft_deleted_secrets_on_destroy      = true
+
+      recover_soft_deleted_key_vaults   = false
+      recover_soft_deleted_certificates = false
+      recover_soft_deleted_keys         = false
+      recover_soft_deleted_secrets      = false
+    }
+  }
 }
 
-# Configuração do provider ACME com o servidor definido no terraform.tfvars
 provider "acme" {
   server_url = var.acme_server_url
 }
